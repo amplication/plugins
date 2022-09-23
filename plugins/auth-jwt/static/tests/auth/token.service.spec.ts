2d7a2bba8e2ea6312e8367d7d72d3bde4c894c64
Amit Barletz <amit@amplication.com>
2022-09-23 20:01:38 +0300

    fix(jwt) fix tests and createToken to satisfy the ITokenPayload interface

--- plugins/auth-jwt/static/tests/auth/token.service.spec.ts
+++ plugins/auth-jwt/static/tests/auth/token.service.spec.ts
@@ -1,14 +1,10 @@
-/* eslint-disable import/no-unresolved */
 import { JwtService } from "@nestjs/jwt";
 import { mock } from "jest-mock-extended";
-//@ts-ignore
 import { TokenServiceBase } from "../../auth/base/token.service.base";
 import {
   INVALID_PASSWORD_ERROR,
   INVALID_USERNAME_ERROR,
-  //@ts-ignore
 } from "../../auth/constants";
-//@ts-ignore
 import { SIGN_TOKEN, VALID_CREDENTIALS } from "./constants";
 
 describe("Testing the TokenServiceBase", () => {
@@ -22,26 +18,27 @@ describe("Testing the TokenServiceBase", () => {
     it("should create valid token for valid username and password", async () => {
       jwtService.signAsync.mockReturnValue(Promise.resolve(SIGN_TOKEN));
       expect(
-        await tokenServiceBase.createToken(
-          VALID_CREDENTIALS.username,
-          VALID_CREDENTIALS.password
-        )
+        await tokenServiceBase.createToken({
+          id: VALID_CREDENTIALS.id,
+          username: VALID_CREDENTIALS.username,
+          password: VALID_CREDENTIALS.password,
+        })
       ).toBe(SIGN_TOKEN);
     });
     it("should reject when username missing", () => {
-      const result = tokenServiceBase.createToken(
-        //@ts-ignore
-        null,
-        VALID_CREDENTIALS.password
-      );
+      const result = tokenServiceBase.createToken({
+        id: VALID_CREDENTIALS.id,
+        username: null,
+        password: VALID_CREDENTIALS.password,
+      });
       return expect(result).rejects.toBe(INVALID_USERNAME_ERROR);
     });
     it("should reject when password missing", () => {
-      const result = tokenServiceBase.createToken(
-        VALID_CREDENTIALS.username,
-        //@ts-ignore
-        null
-      );
+      const result = tokenServiceBase.createToken({
+        id: VALID_CREDENTIALS.id,
+        username: VALID_CREDENTIALS.username,
+        password: null,
+      });
       return expect(result).rejects.toBe(INVALID_PASSWORD_ERROR);
     });
   });
