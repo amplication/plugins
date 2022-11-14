de683a0e57afe1636ab37d41f7ad3c5bacc4a68e
Moshe Forman <96979533+MoFoGo@users.noreply.github.com>
2022-11-14 15:51:58 +0200

    Update index.ts

--- plugins/plugin-template/src/index.ts
+++ plugins/plugin-template/src/index.ts
@@ -32,7 +32,7 @@ class ExamplePlugin implements AmplicationPlugin {
     eventParams: CreateServerParams
   ) {
     // Here you can manipulate the context or save any context variable for your after function.
-    // You can also manipulate the eventParams so it will change the result of Amplication function
+    // You can also manipulate the eventParams so it will change the result of Amplication function.
     // context.utils.skipDefaultBehavior = true; this will prevent the default behavior and skip our handler.
 
     return eventParams // eventParams must return from before function. It will be used for the builder function.
@@ -43,7 +43,7 @@ class ExamplePlugin implements AmplicationPlugin {
     eventParams: CreateServerParams,
     modules: Module[]
   ) {
-    // Here you can get the context, eventParams and the modules Amplication created.
+    // Here you can get the context, eventParams and the modules that Amplication created.
     // Then you can manipulate the modules, add new ones, or create your own.
     const staticPath = resolve(__dirname, "../static");
     const staticsFiles = await context.utils.importStaticModules(
@@ -51,7 +51,7 @@ class ExamplePlugin implements AmplicationPlugin {
       context.serverDirectories.srcDirectory
     );
 
-    return [ ...modules, ...staticsFiles]; // you must return the generated modules you want to generate at this part of the build
+    return [ ...modules, ...staticsFiles]; // You must return the generated modules you want to generate at this part of the build.
   }
 
 
@@ -59,7 +59,7 @@ class ExamplePlugin implements AmplicationPlugin {
     context: DsgContext,
     eventParams: CreateAdminUIParams
   ) {
-    // same as beforeCreateExample but for a different event
+    // Same as beforeCreateExample but for a different event.
 
     return eventParams
   }
