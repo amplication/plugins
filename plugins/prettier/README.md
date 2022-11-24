d851d74d5b456655f8ced119729790ec731ba4a8
Eugene Tseitlin <eugene@amplication.com>
2022-11-24 15:52:22 +0100

    prettier plugin

---
+++ plugins/prettier/README.md
@@ -0,0 +1,17 @@
+# @amplication/plugin-<name>
+
+[![NPM Downloads](https://img.shields.io/npm/dt/@amplication/plugin-auth-basic)](https://www.npmjs.com/package/@amplication/plugin-auth-basic) 
+
+The plugin integrates Prettier to the app.
+
+## Purpose
+
+Provide formatting out of the box by leveraging one of the best and most popular solutions in industry.
+
+## Configuration
+
+Add your formatting rules to .prettierrc file.
+
+# Scripts
+## `format`
+Running `npm run format` will format your code.
---
+++ plugins/prettier/src/index.ts
@@ -0,0 +1,62 @@
+import { resolve } from "path";
+import {
+  DsgContext,
+  AmplicationPlugin,
+  Events,
+  Module,
+  EventNames,
+  CreateServerParams,
+  CreateServerPackageJsonParams,
+} from "@amplication/code-gen-types";
+import { merge } from "lodash";
+
+class ExamplePlugin implements AmplicationPlugin {
+
+  register(): Events {
+    return {
+      [EventNames.CreateServer]: {
+        after: this.afterCreateServer,
+      },
+      [EventNames.CreateServerPackageJson]: {
+        before: this.beforeCreateServerPackageJson,
+      },
+    };
+  }
+
+  async afterCreateServer(
+    context: DsgContext,
+    eventParams: CreateServerParams,
+    modules: Module[]
+  ) {
+    const staticPath = resolve(__dirname, "./static");
+    const staticsFiles = await context.utils.importStaticModules(
+      staticPath,
+      context.serverDirectories.baseDirectory
+    );
+
+    console.log("staticsFiles", staticsFiles);
+    return [...modules, ...staticsFiles];
+  }
+
+  beforeCreateServerPackageJson(
+    context: DsgContext,
+    eventParams: CreateServerPackageJsonParams
+  ): CreateServerPackageJsonParams {
+    const myValues = {
+      devDependencies: {
+        "prettier": "^2.8.0",
+      },
+      scripts: {
+        "format": "prettier --write .",
+      }
+    };
+
+    eventParams.updateProperties.forEach((updateProperty) =>
+      merge(updateProperty, myValues)
+    );
+
+    return eventParams;
+  }
+}
+
+export default ExamplePlugin;
