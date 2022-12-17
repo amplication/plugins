fccb5fd2f67b4aab68c8866be9a15389442f351e
Amit Barletz <amit@amplication.com>
2022-12-17 15:38:12 +0200

    feat(auth-core) create token service

---
+++ plugins/auth-api/core/create-token/create-token-service-test.ts
@@ -0,0 +1,22 @@
+import { print } from "recast";
+import { EnumAuthProviderType } from "../../types";
+import { DsgContext, Module } from "@amplication/code-gen-types";
+import { readFile } from "@amplication/code-gen-utils";
+
+export async function createTokenServiceTests(
+  dsgContext: DsgContext
+): Promise<Module> {
+  const { serverDirectories, resourceInfo } = dsgContext;
+  const authTestsDir = `${serverDirectories.srcDirectory}/tests/auth`;
+  const authProvider: EnumAuthProviderType =
+    resourceInfo?.settings.authProvider || EnumAuthProviderType.Jwt;
+  const name =
+    authProvider === EnumAuthProviderType.Http ? "Basic" : authProvider;
+  const templatePath = require.resolve(
+    `./${name.toLowerCase()}Token.service.spec.template.ts`
+  );
+  const file = await readFile(templatePath);
+  const filePath = `${authTestsDir}/token.service.spec.ts`;
+
+  return { code: print(file).code, path: filePath };
+}
---
+++ plugins/auth-api/core/create-token/create-token-service.ts
@@ -0,0 +1,25 @@
+import { print } from "recast";
+import { DsgContext, Module } from "@amplication/code-gen-types";
+import { removeTSIgnoreComments } from "../../util/ast";
+import { readFile } from "@amplication/code-gen-utils";
+import { EnumAuthProviderType } from "../../types";
+
+export async function createTokenService(
+  dsgContext: DsgContext
+): Promise<Module> {
+  const { serverDirectories, resourceInfo } = dsgContext;
+  const authProvider: EnumAuthProviderType =
+    resourceInfo?.settings.authProvider || EnumAuthProviderType.Jwt;
+  const authDir = `${serverDirectories.srcDirectory}/auth`;
+  const name =
+    authProvider === EnumAuthProviderType.Http ? "Basic" : authProvider;
+  const templatePath = require.resolve(
+    `./${name.toLowerCase()}Token.service.template.ts`
+  );
+  const file = await readFile(templatePath);
+  const filePath = `${authDir}/base/token.service.base.ts`;
+
+  removeTSIgnoreComments(file);
+
+  return { code: print(file).code, path: filePath };
+}
