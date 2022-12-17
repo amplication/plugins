046e6d63edbb9e26b3e4fce5254983f1d981d6db
Amit Barletz <amit@amplication.com>
2022-12-17 15:39:33 +0200

    feat(auth-core) install code gen utils and use it

--- plugins/auth-api/util/module.ts
+++ plugins/auth-api/util/module.ts
@@ -1,13 +1,29 @@
-import { memoize } from "lodash";
-import * as fs from "fs";
-import { namedTypes } from "ast-types";
-import { parse } from "./ast";
+import path from 'path';
+import normalize from "normalize-path";
 
-export const readCode = memoize((path: string): Promise<string> => {
-  return fs.promises.readFile(path, "utf-8");
-});
+const JSON_EXT = ".json";
 
-export const readFile = async (path: string): Promise<namedTypes.File> => {
-  const code = await readCode(path);
-  return parse(code) as namedTypes.File;
-};
+/**
+ * @param from filePath of the module to import from
+ * @param to filePath of the module to import to
+ */
+export function relativeImportPath(from: string, to: string): string {
+  const relativePath = path.relative(path.dirname(from), to);
+  return filePathToModulePath(relativePath);
+}
+
+/**
+ * @param filePath path to the file to import
+ * @returns module path of the given file path
+ */
+export function filePathToModulePath(filePath: string): string {
+  const parsedPath = path.parse(filePath);
+  const fixedExtPath =
+    parsedPath.ext === JSON_EXT
+      ? filePath
+      : path.join(parsedPath.dir, parsedPath.name);
+  const normalizedPath = normalize(fixedExtPath);
+  return normalizedPath.startsWith("/") || normalizedPath.startsWith(".")
+    ? normalizedPath
+    : "./" + normalizedPath;
+}
