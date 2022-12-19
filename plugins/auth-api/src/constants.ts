4c5adfbf1d1a617811a84cd26641ff53628cd111
Mor Hagbi <mor@amplication.com>
2022-12-19 08:40:22 +0200

    delete unrelevant files

--- plugins/auth-api/src/constants.ts
+++
@@ -1,6 +0,0 @@
-import { VariableDictionary } from "@amplication/code-gen-types";
-
-export const envVariables: VariableDictionary = [
-  { JWT_SECRET_KEY: "Change_ME!!!" },
-  { JWT_EXPIRATION: "2d" },
-];
--- plugins/auth-api/src/types.ts
+++
@@ -1,4 +0,0 @@
-export enum EnumAuthProviderType {
-  Http = "Http",
-  Jwt = "Jwt",
-}
