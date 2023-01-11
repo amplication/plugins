a8e9e13a6ce10edfde4372a22c7b8aa3ea05d21b
Mor Hagbi <mor@amplication.com>
2023-01-11 20:47:43 +0200

    Add CreateSeed event

--- plugins/auth-api/src/index.ts
+++ plugins/auth-api/src/index.ts
@@ -8,6 +8,7 @@ import {
   CreateEntityResolverToOneRelationMethodsParams,
   CreateEntityServiceBaseParams,
   CreateEntityServiceParams,
+  CreateSeedParams,
   CreateServerDotEnvParams,
   CreateServerPackageJsonParams,
   DsgContext,
@@ -31,11 +32,9 @@ import {
   addDecoratorsToClassDeclaration,
   addIdentifierToConstructorSuperCall,
   addImports,
-  awaitExpression,
   getClassDeclarationById,
   importNames,
   interpolate,
-  logicalExpression,
   memberExpression,
 } from "./util/ast";
 import { isPasswordField } from "./util/field";
@@ -48,6 +47,14 @@ import {
 } from "./core/create-method-id-action-entity-map";
 import { relativeImportPath } from "./util/module";
 import { addInjectableDependency } from "./util/nestjs-code-generation";
+import {
+  BlockStatement,
+  IfStatement,
+  Statement,
+  FunctionDeclaration,
+  Identifier,
+  TSTypeAnnotation,
+} from "@babel/types";
 
 const TO_MANY_MIXIN_ID = builders.identifier("Mixin");
 const ARGS_ID = builders.identifier("args");
@@ -95,6 +102,9 @@ class AuthCorePlugin implements AmplicationPlugin {
       },
       CreateEntityServiceBase: {
         before: this.beforeCreateEntityServiceBase,
+      },
+      CreateSeed: {
+        before: this.beforeCreateSeed,
       },
     };
   }
@@ -307,20 +317,20 @@ class AuthCorePlugin implements AmplicationPlugin {
       ) as namedTypes.ImportDeclaration[]
     );
 
-    // const guardDecorator = builders.decorator(
-    //   builders.callExpression(
-    //     builders.memberExpression(
-    //       builders.identifier("common"),
-    //       builders.identifier("UseGuards")
-    //     ),
-    //     [builders.identifier("DefaultAuthGuard")]
-    //   )
-    // );
+    const guardDecorator = builders.decorator(
+      builders.callExpression(
+        builders.memberExpression(
+          builders.identifier("common"),
+          builders.identifier("UseGuards")
+        ),
+        [builders.identifier("DefaultAuthGuard")]
+      )
+    );
 
-    // const classDeclarationWithClassDecorator = addDecoratorsToClassDeclaration(
-    //   classDeclaration,
-    //   [guardDecorator]
-    // );
+    const classDeclarationWithClassDecorator = addDecoratorsToClassDeclaration(
+      classDeclaration,
+      [guardDecorator]
+    );
 
     if (classDeclaration) {
       controllerMethodsIdsActionPairs(templateMapping, entity).forEach(
@@ -550,6 +560,156 @@ class AuthCorePlugin implements AmplicationPlugin {
         ),
       ]);
     }
+    return eventParams;
+  }
+
+  beforeCreateSeed(context: DsgContext, eventParams: CreateSeedParams) {
+    interpolate(eventParams.template, eventParams.templateMapping);
+
+    const passwordImport = importNames(
+      [builders.identifier("Salt"), builders.identifier("parseSalt")],
+      "../src/auth/password.service"
+    );
+
+    const hashImport = importNames([builders.identifier("hash")], "bcrypt");
+
+    addImports(
+      eventParams.template,
+      [passwordImport, hashImport].filter(
+        (x) => x //remove nulls and undefined
+      ) as namedTypes.ImportDeclaration[]
+    );
+
+    const ifStatementFromBody = eventParams.template.program.body.find(
+      (x) => x.type === "IfStatement"
+    ) as IfStatement;
+
+    const functionDeclarationFromBody = eventParams.template.program.body.find(
+      (x) => x.type === "FunctionDeclaration"
+    ) as FunctionDeclaration;
+
+    const ifBlock = ifStatementFromBody.consequent as BlockStatement;
+    const functionDeclarationBlock =
+      functionDeclarationFromBody.body as BlockStatement;
+
+    const saltConstVariable = builders.variableDeclaration("const", [
+      builders.variableDeclarator(
+        builders.identifier("salt"),
+        builders.callExpression(builders.identifier("parseSalt"), [
+          builders.identifier("BCRYPT_SALT"),
+        ])
+      ),
+    ]) as Statement;
+
+    const blockCode: namedTypes.BlockStatement = {
+      body: [
+        builders.expressionStatement(
+          builders.callExpression(
+            builders.memberExpression(
+              builders.identifier("console"),
+              builders.identifier("error")
+            ),
+            [builders.identifier("error")]
+          )
+        ),
+        builders.expressionStatement(
+          builders.callExpression(
+            builders.memberExpression(
+              builders.identifier("process"),
+              builders.identifier("exit")
+            ),
+            [builders.numericLiteral(1)]
+          )
+        ),
+      ],
+      directives: [],
+      type: "BlockStatement",
+    };
+
+    const saltExpression = builders.expressionStatement(
+      builders.callExpression(
+        builders.memberExpression(
+          builders.callExpression(builders.identifier("seed"), [
+            builders.identifier("salt"),
+          ]),
+          builders.identifier("catch")
+        ),
+        [
+          builders.arrowFunctionExpression(
+            [builders.identifier("error")],
+            blockCode
+          ),
+        ]
+      )
+    ) as Statement;
+
+    const bcryptSaltIdentifier = builders.identifier(
+      "bcryptSalt"
+    ) as Identifier;
+    bcryptSaltIdentifier.typeAnnotation = builders.tsTypeAnnotation(
+      builders.tsTypeReference(builders.identifier("Salt"))
+    ) as TSTypeAnnotation;
+
+    const functionExp = builders.expressionStatement(
+      builders.awaitExpression(
+        builders.callExpression(
+          builders.memberExpression(
+            builders.memberExpression(
+              builders.identifier("client"),
+              builders.identifier("user")
+            ),
+            builders.identifier("upsert")
+          ),
+          [
+            builders.objectExpression([
+              builders.objectProperty(
+                builders.identifier("where"),
+                builders.objectExpression([
+                  builders.objectProperty(
+                    builders.identifier("username"),
+                    builders.memberExpression(
+                      builders.identifier("data"),
+                      builders.identifier("username")
+                    )
+                  ),
+                ])
+              ),
+              builders.objectProperty(
+                builders.identifier("update"),
+                builders.objectExpression([])
+              ),
+              builders.objectProperty(
+                builders.identifier("create"),
+                builders.identifier("data")
+              ),
+            ]),
+          ]
+        )
+      )
+    ) as Statement;
+
+    const dataVar = builders.variableDeclaration("const", [
+      builders.variableDeclarator(
+        builders.identifier("data"),
+        builders.identifier("DATA")
+      ),
+    ]) as Statement;
+
+    functionDeclarationFromBody.params.push(bcryptSaltIdentifier);
+    const prevStatement2 = functionDeclarationBlock.body[2];
+    const prevStatement3 = functionDeclarationBlock.body[3];
+    const prevStatement4 = functionDeclarationBlock.body[4];
+    const prevStatement5 = functionDeclarationBlock.body[5];
+
+    functionDeclarationBlock.body[2] = dataVar;
+    functionDeclarationBlock.body[3] = functionExp;
+    functionDeclarationBlock.body[4] = prevStatement2;
+    functionDeclarationBlock.body[5] = prevStatement3;
+    functionDeclarationBlock.body[6] = prevStatement4;
+    functionDeclarationBlock.body[7] = prevStatement5;
+
+    ifBlock.body.push(saltConstVariable, saltExpression);
+
     return eventParams;
   }
 
