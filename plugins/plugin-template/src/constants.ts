2ebd5b1cab163c8c345cc47c12ba6b5beca006e5
tupe12334 <ofek.gabay.he@gmail.com>
2023-01-16 14:33:25 +0200

    add templates to constants

--- plugins/plugin-template/src/constants.ts
+++ plugins/plugin-template/src/constants.ts
@@ -3,4 +3,9 @@
  * Add all your constants here.
  */
 
+import { join } from "path";
+
 export const ExampleConst = "example";
+
+export const staticsPath = join(__dirname, "static");
+export const templatesPath = join(__dirname, "templates");
