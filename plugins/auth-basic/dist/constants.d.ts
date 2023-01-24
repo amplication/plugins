0f76b0451871f78eb3aed3d5caceccf1d168e353
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-01-24 16:08:22 +0000

    chore: remove redundant files

--- plugins/auth-basic/dist/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const ExampleConst = "example";
-export declare const staticsPath: string;
-export declare const templatesPath: string;
--- plugins/auth-jwt/dist/partial-parser.d.ts
+++
@@ -1,5 +0,0 @@
-import type { File } from "@babel/types";
-import { Overrides } from "recast/parsers/_babel_options";
-import * as parser from "./parser";
-export declare function parse(source: string, options?: Overrides): File;
-export declare function getOptions(options?: Overrides): parser.Options;
