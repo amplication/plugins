70b7d7c5e2d8e00b157849b55a0900b2586eb5b5
Mor Hagbi <mor@amplication.com>
2023-02-08 10:46:49 +0200

    fix(auth-core): remove dists folders

--- plugins/auth-basic/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { DsgContext, CreateServerAuthParams, AmplicationPlugin, Events, CreateAdminUIParams } from "@amplication/code-gen-types";
-declare class BasicAuthPlugin implements AmplicationPlugin {
-    register(): Events;
-    beforeCreateAdminModules(context: DsgContext, eventParams: CreateAdminUIParams): CreateAdminUIParams;
-    beforeCreateAuthModules(context: DsgContext, eventParams: CreateServerAuthParams): CreateServerAuthParams;
-    afterCreateAuthModules(context: DsgContext, eventParams: CreateServerAuthParams): Promise<import("@amplication/code-gen-types").Module[]>;
-}
-export default BasicAuthPlugin;
