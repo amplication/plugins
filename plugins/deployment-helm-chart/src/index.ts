38ffef8acd1933f2f99af93fa9dc952e7886a671
levivannoort <levivnoort@gmail.com>
2023-04-14 15:30:15 +0200

    chore(deployment-helm-chart): regex on the service name to prevent unrenderable names and addition of host setting

--- plugins/deployment-helm-chart/src/index.ts
+++ plugins/deployment-helm-chart/src/index.ts
@@ -1,16 +1,15 @@
 import type {
   AmplicationPlugin,
   CreateServerDotEnvParams,
-  CreateAdminUIParams,
   DsgContext,
   Events,
   Module,
-  PluginInstallation,
 } from "@amplication/code-gen-types";
 import {
   applicationVersionKey,
   chartVersionKey,
   configurationKey,
+  hostKey,
   repositoryKey,
   serviceNameKey,
   tagKey,
@@ -19,7 +18,6 @@ import { join } from "node:path";
 import { getPluginSettings } from "./utils";
 import { resolve } from "path";
 import { EventNames } from "@amplication/code-gen-types";
-import { parse, stringify } from "yaml";
 
 class HelmChartPlugin implements AmplicationPlugin {
   register(): Events {
@@ -27,8 +25,8 @@ class HelmChartPlugin implements AmplicationPlugin {
       [EventNames.CreateServerDotEnv]: {
         after: this.afterCreateServerDotEnv,
       },
-      // [EventNames.CreateAdminUI]: {
-      //   after: this.afterCreateAdminUI,
+      // [EventNames.CreateAdminUIDotEnv]: {
+      //   after: this.afterCreateAdminUIDotEnv,
       // },
     };
   }
@@ -39,10 +37,11 @@ class HelmChartPlugin implements AmplicationPlugin {
     modules: Module[]
   ) {
     context.logger.info(`Generating Helm Chart...`);
+
     // determine the name of the service which will be used as the name for the helm chart
     const serviceName = context.resourceInfo?.name
       .toLocaleLowerCase()
-      .replaceAll(" ", "-");
+      .replaceAll(/[^a-zA-Z0-9-]/, "-");
 
     // chart names must be lower case letters and numbers. words may be separated with dashes (-):
     // to-do: write logic that removes anything that isnt a letters, number or dash
@@ -54,7 +53,7 @@ class HelmChartPlugin implements AmplicationPlugin {
     const settings = getPluginSettings(context.pluginInstallations);
 
     // fetch the variables from the .env file and create a variable for it which can be
-    // used to fill the configmap secrets should be moved by the users themself
+    // used to fill the configmap, secrets should be moved by the users themself
     const variables = eventParams.envVariables;
 
     if (!variables) {
@@ -73,16 +72,18 @@ class HelmChartPlugin implements AmplicationPlugin {
       }
     });
 
-    // save the renderedOutput to the desired directory the options are on the root of the repository
-    // and within the directory of the services itself setting "root_directory"
-    // option 1 (value: true):  /<directory_name_value>/<service_name>
-    // create directory on the root of the repository (../) with the name provided
-    // through the settings.directory_name, subsequently place all of the files that
-    // from the static directory via the renderdOutput variable
-    // option 2 (value: false): /<service_name>/<directory_name_value>/<service_name>
-    // create directory within the directory of the service with the name provided
-    // through the settings.directory_name, subsequently place all of the files that
-    // from the static directory via the renderdOutput variable
+    /**
+     * save the renderedOutput to the desired directory the options are on the root of the repository
+     * and within the directory of the services itself setting "root_directory":
+     *    option 1 (value: true):  /<directory_name_value>/<service_name>
+     *         create directory on the root of the repository (../) with the name provided
+     *         through the settings.directory_name, subsequently place all of the files that
+     *         from the static directory via the renderdOutput variable
+     *    option 2 (value: false): /<service_name>/<directory_name_value>/<service_name>
+     *         create directory within the directory of the service with the name provided
+     *         through the settings.directory_name, subsequently place all of the files that
+     *         from the static directory via the renderdOutput variable
+     */
     let helmDirectoryPath = "";
     if (settings.root_level == true) {
       helmDirectoryPath = join(
@@ -118,69 +119,19 @@ class HelmChartPlugin implements AmplicationPlugin {
           .replace(applicationVersionKey, settings.server.application_version)
           .replace(repositoryKey, settings.server.repository)
           .replace(tagKey, settings.server.tag)
+          .replace(hostKey, settings.server.host)
           .replace(configurationKey, configmap),
       })
     );
 
-    context.logger.info("Configuring Helm chart template");
+    context.logger.info("Configuring Helm chart template...");
     return [...modules, ...renderdOutput];
   }
 
-  // async afterCreateAdminUI(
-  //   context: DsgContext,
-  //   eventParams: CreateAdminUIParams,
-  //   modules: Module[]
-  // ) {
-  //   const staticPath = resolve(__dirname, "./static");
-  //   const staticsFiles = await context.utils.importStaticModules(
-  //     staticPath,
-  //     context.serverDirectories.srcDirectory
-  //   );
-
-  //   // determine the name of the service which will be used
-  //   // as the name for the helm chart
-  //   const serviceName = context.resourceInfo?.name
-  //     .toLocaleLowerCase()
-  //     .replaceAll(" ", "-");
-
-  //   // fetch user settings + merge with default settings
-  //   const settings = getPluginSettings(context.pluginInstallations);
-
-  //   const settings = merge(defaultSettings, userSettings);
-
-  //   if (settings.admin_ui.enabled == false) {
-  //     // throw info message "Not generating helm chart for admin-ui as admin_ui.enabled is false"
-  //   } else if (settings.admin_ui.enabled == true) {
-  //     // fetch the variables from the .env file and create a variable for it which can be
-  //     // used to fill the configmap secrets should be moved by the users themself
-  //     // const variables = eventParams.envVariables;
-
-  //     if (!variables) {
-  //       throw new Error("Variables can't be fetched");
-  //     }
-
-  //     // render the helm chart from the static files
-  //     // in combination with the values provided through
-  //     // the settings
-  //     const renderdOutput = staticsFiles
-  //       .map(
-  //         (file): Module => ({
-  //           path: file.path,
-  //           code: file.code
-  //             .replaceAll(serviceNameKey, serviceName)
-  //             .replace(chartVersionKey, settings.admin_ui.chart_version)
-  //             .replace(applicationVersionKey, settings.admin_ui.application_version)
-  //             .replace(repositoryKey, settings.admin_ui.repository)
-  //             .replace(tagKey, settings.admin_ui.tag)
-  //             .replace(configurationKey, variables)
-  //         })
-  //       );
-
-  //     return [...modules, ...renderdOutput];
-  //   } else {
-  //     throw new Error("The setting admin_ui needs to be either 'true' or 'false' ");
-  //   }
-  // }
+  /**
+   * Implementation of the AdminUI part of generation of a helm Chart requires the
+   * 'afterCreateAdminUIDotEnv' event to be implemented.
+   */
 }
 
 export default HelmChartPlugin;
