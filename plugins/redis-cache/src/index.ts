7c1349f96ac0cebdae7479e235e05378c770ae69
demilade <sonugademilade8703@gmail.com>
2023-08-05 16:52:57 +0100

    the plugin now adds the necessary code to the server app module for caching to work

--- plugins/redis-cache/src/index.ts
+++ plugins/redis-cache/src/index.ts
@@ -1,11 +1,15 @@
 import type {
   AmplicationPlugin,
+  CreateServerAppModuleParams,
   CreateServerPackageJsonParams,
   DsgContext,
   Events
 } from "@amplication/code-gen-types";
 import { EventNames } from "@amplication/code-gen-types";
 import { merge, update } from "lodash"
+import { addImport } from "./utils";
+import * as utils from "./utils"
+import { builders, namedTypes } from "ast-types"
 
 
 class RedisCachePlugin implements AmplicationPlugin {
@@ -13,6 +17,9 @@ class RedisCachePlugin implements AmplicationPlugin {
     return {
       [EventNames.CreateServerPackageJson]: {
         before: this.beforeCreateServerPackageJson
+      },
+      [EventNames.CreateServerAppModule]: {
+        before: this.beforeCreateServerAppModule
       }
     };
   }
@@ -36,6 +43,45 @@ class RedisCachePlugin implements AmplicationPlugin {
 
     return eventParams;
   }
+
+  beforeCreateServerAppModule(
+    context: DsgContext,
+    eventParams: CreateServerAppModuleParams
+  ): CreateServerAppModuleParams {
+    const { template, templateMapping } = eventParams;
+
+    utils.addImport(template, stmt('import { CacheModule } from "@nestjs/common";'))
+    utils.addImport(template, stmt('import * as redisStore from "cache-manager-redis-store"'))
+
+    if(!templateMapping["MODULES"]) {
+      throw new Error("Failed to find the app module's imported modules")
+    }
+
+    const modules = templateMapping.MODULES as namedTypes.ArrayExpression;
+    const cacheModule = cacheModuleInstantiation()
+    modules.elements.push(cacheModule)
+
+    return eventParams
+  }
+}
+
+const stmt = (stmt: string): any => {
+  return utils.parse(stmt).program.body[0]
+}
+
+const cacheModuleInstantiation = () => {
+  return builders.callExpression(
+    builders.memberExpression(
+      builders.identifier("CacheModule"),
+      builders.identifier("register")
+    ),
+    [builders.objectExpression([
+      builders.objectProperty(
+        builders.identifier("isGlobal"),
+        builders.booleanLiteral(true)
+      )
+    ])]
+  );
 }
 
 export default RedisCachePlugin;
