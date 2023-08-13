ec528c99a86642e6924859135bd997894c436e7d
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-08-14 01:52:12 +0530

    feat: add settings functionality

--- plugins/formatter-prettier/src/index.ts
+++ plugins/formatter-prettier/src/index.ts
@@ -9,44 +9,76 @@ import {
   CreateServerPackageJsonParams,
   CreateAdminUIPackageJsonParams,
   ModuleMap,
+  CreateAdminUIParams,
 } from "@amplication/code-gen-types";
-import { merge } from "lodash";
+import { getPluginSettings } from "./utils";
+import { format } from "prettier";
 
-class ExamplePlugin implements AmplicationPlugin {
+class PrettierPlugin implements AmplicationPlugin {
   register(): Events {
     return {
       [EventNames.CreateServer]: {
-        after: this.afterCreateServer,
+        after: this.afterCreateApp("server").bind(this),
+      },
+      [EventNames.CreateAdminUI]: {
+        after: this.afterCreateApp("client").bind(this),
       },
       [EventNames.CreateServerPackageJson]: {
-        before: this.beforeCreateServerPackageJson,
+        before: this.beforeCreatePackageJson,
       },
       [EventNames.CreateAdminUIPackageJson]: {
-        before: this.beforeCreateClientPackageJson,
+        before: this.beforeCreatePackageJson,
       },
     };
   }
 
-  async afterCreateServer(
-    context: DsgContext,
-    eventParams: CreateServerParams,
-    modules: ModuleMap
-  ): Promise<ModuleMap> {
-    const staticPath = resolve(__dirname, "./static");
-    const staticsFiles = await context.utils.importStaticModules(
-      staticPath,
-      context.serverDirectories.baseDirectory
-    );
+  afterCreateApp = (
+    event: "server" | "client",
+  ) => {
+    return async (
+      context: DsgContext,
+      eventParams: CreateAdminUIParams | CreateServerParams,
+      modules: ModuleMap,
+    ): Promise<ModuleMap> => {
+      const { rules } = getPluginSettings(
+        context.pluginInstallations
+      );
+      let baseDirectory;
+      const staticFilesPath = resolve(__dirname, "static");
+      const rulesPlaceholder = "\"${{ RULES }}\"";
 
-    await modules.merge(staticsFiles);
-    return modules;
-  }
+      console.log("rules", rules);
+    
+      switch (event) {
+        case "server":
+          baseDirectory = context.serverDirectories.baseDirectory;
+          break;
+  
+        case "client":
+          baseDirectory = context.clientDirectories.baseDirectory;
+          break;
+      }
+  
+      const staticFiles = await context.utils.importStaticModules(
+        staticFilesPath,
+        baseDirectory
+      );
+      staticFiles.replaceModulesCode(( code ) => 
+        code.replaceAll(
+          rulesPlaceholder, 
+          format(JSON.stringify(rules, null, 2), { parser: "json" })
+        ));
+    
+      await modules.merge(staticFiles);
+      return modules;
+    };
+  };
 
-  beforeCreateServerPackageJson(
+  beforeCreatePackageJson(
     context: DsgContext,
-    eventParams: CreateServerPackageJsonParams
-  ): CreateServerPackageJsonParams {
-    const myValues = {
+    eventParams: CreateServerPackageJsonParams | CreateAdminUIPackageJsonParams
+  ): CreateServerPackageJsonParams | CreateAdminUIPackageJsonParams {
+    const packageJsonValues = {
       devDependencies: {
         prettier: "^2.8.0",
       },
@@ -55,32 +87,9 @@ class ExamplePlugin implements AmplicationPlugin {
       },
     };
 
-    eventParams.updateProperties.forEach((updateProperty) =>
-      merge(updateProperty, myValues)
-    );
-
-    return eventParams;
-  }
-
-  beforeCreateClientPackageJson(
-    context: DsgContext,
-    eventParams: CreateAdminUIPackageJsonParams
-  ): CreateAdminUIPackageJsonParams {
-    const myValues = {
-      devDependencies: {
-        prettier: "^2.8.0",
-      },
-      scripts: {
-        format: "prettier --write .",
-      },
-    };
-
-    eventParams.updateProperties.forEach((updateProperty) =>
-      merge(updateProperty, myValues)
-    );
-
+    eventParams.updateProperties.push(packageJsonValues);  
     return eventParams;
   }
 }
 
-export default ExamplePlugin;
+export default PrettierPlugin;
