d1d6f9c21a06238629c8e712f7642d721d3c1c82
mulygottlieb <muly@amplication.com>
2023-09-11 07:27:35 +0300

    feat(plugins): sql-server, add onDelete onUpdate to relations

--- plugins/db-sqlserver/src/index.ts
+++ plugins/db-sqlserver/src/index.ts
@@ -6,13 +6,25 @@ import {
   CreateServerDotEnvParams,
   CreateServerParams,
   DsgContext,
+  Entity,
+  EntityField,
   EnumDataType,
   Events,
+  LookupResolvedProperties,
   ModuleMap,
+  types,
 } from "@amplication/code-gen-types";
-import { resolve } from "path";
 import { getPluginSettings } from "./utils";
-import { dataSource, updateDockerComposeDevProperties, updateDockerComposeProperties } from "./constants";
+import {
+  dataSource,
+  updateDockerComposeDevProperties,
+  updateDockerComposeProperties,
+} from "./constants";
+import { ReferentialActions, ScalarType } from "prisma-schema-dsl-types";
+import * as PrismaSchemaDSL from "prisma-schema-dsl";
+import * as PrismaSchemaDSLTypes from "prisma-schema-dsl-types";
+import { camelCase } from "camel-case";
+import { pascalCase } from "pascal-case";
 
 class MSSQLServerPlugin implements AmplicationPlugin {
   register(): Events {
@@ -51,18 +63,28 @@ class MSSQLServerPlugin implements AmplicationPlugin {
 
     context.entities?.forEach(({ name: entityName, fields }) => {
       const enumField = fields.find(
-        ({ dataType }) => dataType === EnumDataType.MultiSelectOptionSet || dataType === EnumDataType.OptionSet
+        ({ dataType }) =>
+          dataType === EnumDataType.MultiSelectOptionSet ||
+          dataType === EnumDataType.OptionSet
       );
       if (enumField) {
-        const errorMessage = generateErrorMessageForEnums(enumField.dataType as string, entityName, enumField.name);
+        const errorMessage = generateErrorMessageForEnums(
+          enumField.dataType as string,
+          entityName,
+          enumField.name
+        );
         context.logger.error(errorMessage);
         throw new Error(errorMessage);
       }
 
       const jsonField = fields.find(
-        ({ dataType }) => dataType === EnumDataType.Json);
+        ({ dataType }) => dataType === EnumDataType.Json
+      );
       if (jsonField) {
-        const errorMessage = generateErrorMessageForJson(entityName, jsonField.name);
+        const errorMessage = generateErrorMessageForJson(
+          entityName,
+          jsonField.name
+        );
         context.logger.error(errorMessage);
         throw new Error(errorMessage);
       }
@@ -127,11 +149,171 @@ class MSSQLServerPlugin implements AmplicationPlugin {
       });
     });
 
+    eventParams.createFieldsHandlers[EnumDataType.Lookup] = (
+      field: EntityField,
+      entity: Entity,
+      fieldNamesCount: Record<string, number> = {}
+    ) => {
+      const { name, properties } = field;
+      const {
+        relatedEntity,
+        relatedField,
+        allowMultipleSelection,
+        isOneToOneWithoutForeignKey,
+      } = properties as LookupResolvedProperties;
+      const hasAnotherRelation = entity.fields.some(
+        (entityField) =>
+          entityField.id !== field.id &&
+          entityField.dataType === EnumDataType.Lookup &&
+          entityField.properties.relatedEntity.name === relatedEntity.name
+      );
+
+      const relationName = !hasAnotherRelation
+        ? null
+        : MSSQLServerPlugin.createRelationName(
+            entity,
+            field,
+            relatedEntity,
+            relatedField,
+            fieldNamesCount[field.name] === 1,
+            fieldNamesCount[relatedField.name] === 1
+          );
+
+      if (allowMultipleSelection || isOneToOneWithoutForeignKey) {
+        return [
+          PrismaSchemaDSL.createObjectField(
+            name,
+            relatedEntity.name,
+            !isOneToOneWithoutForeignKey,
+            allowMultipleSelection || false,
+            relationName,
+            undefined,
+            undefined,
+            ReferentialActions.NoAction,
+            ReferentialActions.NoAction
+          ),
+        ];
+      }
+
+      const scalarRelationFieldName = field.properties.fkFieldName;
+
+      const relatedEntityFiledId = relatedEntity.fields?.find(
+        (relatedEntityField) => relatedEntityField.dataType === EnumDataType.Id
+      );
+
+      const idType =
+        (relatedEntityFiledId?.properties as types.Id)?.idType ?? "CUID";
+
+      return [
+        PrismaSchemaDSL.createObjectField(
+          name,
+          relatedEntity.name,
+          false,
+          field.required,
+          relationName,
+          [scalarRelationFieldName],
+          [
+            "id",
+          ] /**@todo: calculate the referenced field on the related entity (currently it is always 'id') */,
+          ReferentialActions.NoAction,
+          ReferentialActions.NoAction,
+          undefined,
+          field.customAttributes || undefined 
+        ),
+        // Prisma Scalar Relation Field
+        PrismaSchemaDSL.createScalarField(
+          scalarRelationFieldName,
+          MSSQLServerPlugin.idTypeToPrismaScalarType[idType],
+          false,
+          field.required,
+          !field.properties.allowMultipleSelection &&
+            !relatedField?.properties.allowMultipleSelection &&
+            !isOneToOneWithoutForeignKey
+            ? true
+            : field.unique,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          field.customAttributes || undefined
+        ),
+      ];
+    };
+
     return {
       ...eventParams,
       dataSource: dataSource,
     };
   }
+
+  /**
+   * Creates Prisma Schema relation name according to the names of the entity,
+   * field, relatedEntity and relatedField.
+   * This function is assumed to be used when a relation name is necessary
+   * @param entity
+   * @param field
+   * @param relatedEntity
+   * @param relatedField
+   * @param fieldHasUniqueName
+   * @returns Prisma Schema relation name
+   * @todo use unique name of one of the fields deterministically (VIPCustomers or VIPOrganizations)
+   */
+  private static createRelationName(
+    entity: Entity,
+    field: EntityField,
+    relatedEntity: Entity,
+    relatedField: EntityField,
+    fieldHasUniqueName: boolean,
+    relatedFieldHasUniqueName: boolean
+  ): string {
+    const relatedEntityNames = [
+      relatedEntity.name,
+      pascalCase(relatedEntity.pluralName),
+    ];
+    const entityNames = [entity.name, pascalCase(entity.pluralName)];
+    const matchingRelatedEntityName = relatedEntityNames.find(
+      (name) => field.name === camelCase(name)
+    );
+    const matchingEntityName = entityNames.find(
+      (name) => relatedField.name === camelCase(name)
+    );
+    if (matchingRelatedEntityName && matchingEntityName) {
+      const names = [matchingRelatedEntityName, matchingEntityName];
+      // Sort names for deterministic results regardless of entity and related order
+      names.sort();
+      return names.join("On");
+    }
+    if (fieldHasUniqueName || relatedFieldHasUniqueName) {
+      const names = [];
+      if (fieldHasUniqueName) {
+        names.push(field.name);
+      }
+      if (relatedFieldHasUniqueName) {
+        names.push(relatedField.name);
+      }
+      // Sort names for deterministic results regardless of entity and related order
+      names.sort();
+      return names[0];
+    }
+    const entityAndField = [entity.name, field.name].join(" ");
+    const relatedEntityAndField = [relatedEntity.name, relatedField.name].join(
+      " "
+    );
+    const parts = [entityAndField, relatedEntityAndField];
+    // Sort parts for deterministic results regardless of entity and related order
+    parts.sort();
+    return pascalCase(parts.join(" "));
+  }
+
+  private static idTypeToPrismaScalarType: {
+    [key in types.Id["idType"]]: PrismaSchemaDSLTypes.ScalarType;
+  } = {
+    AUTO_INCREMENT: PrismaSchemaDSLTypes.ScalarType.Int,
+    AUTO_INCREMENT_BIG_INT: PrismaSchemaDSLTypes.ScalarType.BigInt,
+    CUID: PrismaSchemaDSLTypes.ScalarType.String,
+    UUID: PrismaSchemaDSLTypes.ScalarType.String,
+  };
 }
 
 export default MSSQLServerPlugin;
