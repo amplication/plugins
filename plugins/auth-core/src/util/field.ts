73b18fa795e9f7be33bc28a6f5769624ea0b2e19
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-10-03 19:12:07 +0530

    chore: remove unnecessary password service in auth-entity

--- plugins/auth-core/src/util/field.ts
+++
@@ -1,5 +0,0 @@
-import { EntityField, EnumDataType } from "@amplication/code-gen-types";
-
-export function isPasswordField(field: EntityField): boolean {
-  return field.dataType === EnumDataType.Password;
-}
