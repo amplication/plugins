6d40d5b1357f1018a84fce6e9e4d393c9dcbf6cf
levivannoort <levivnoort@gmail.com>
2023-10-12 16:05:46 +0200

    chore: more values on settings interface

--- plugins/ci-github-actions-aws-ecs/src/types.ts
+++ plugins/ci-github-actions-aws-ecs/src/types.ts
@@ -1,7 +1,12 @@
 export interface Settings {
   region: string;
+  account_id: number;
   ecr_repository_name: string;
   ecs_cluster_name: string;
   ecs_service_name: string;
   iam_role_name: string;
+  resources: {
+    cpu: number;
+    memory: number
+  }
 }
