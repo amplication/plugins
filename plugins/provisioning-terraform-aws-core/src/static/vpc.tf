d21315ca6c8453aba3dced6d2337e93cd33b312a
levivannoort <levivnoort@gmail.com>
2023-10-23 14:01:44 +0200

    chore: fix the concat function on the vpc name

--- plugins/provisioning-terraform-aws-core/src/static/vpc.tf
+++ plugins/provisioning-terraform-aws-core/src/static/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = concat("vpc-", local.name)
+  name = join("-", ["vpc", local.name])
   cidr = local.vpc_cidr_block
 
   azs              = local.vpc_azs
