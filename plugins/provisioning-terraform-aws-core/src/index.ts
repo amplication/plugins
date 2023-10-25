620baee0cc3a42a0de759edcbf9098982a4744b6
levivannoort <levivnoort@gmail.com>
2023-10-25 19:57:12 +0200

    fix default error case

--- plugins/provisioning-terraform-aws-core/src/index.ts
+++ plugins/provisioning-terraform-aws-core/src/index.ts
@@ -78,10 +78,6 @@ class TerraformAwsCorePlugin implements AmplicationPlugin {
         backendConfiguration = `terraform {\n\tbackend "local" {\n\t\tpath = "${settings.backend?.local?.path}"\n\t}\n}`;
       case BackendTypes.S3:
         backendConfiguration = `terraform {\n\tbackend "s3" {\n\t\tbucket = "${settings.backend?.s3?.bucket_name}"\n\t\tkey    = "${settings.backend?.s3?.key}"\n\t\tregion = "${settings.backend?.s3?.region}"\n\t}\n}`;
-      default:
-        throw new Error(
-          "TerraformAwsCorePlugin: Specify a backend type and applicable subconfiguration"
-        );
     }
 
     // set the path to the static files and fetch them for manipulation
