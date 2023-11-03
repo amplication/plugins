1fb5888b4fd867bc86f49e2919c737b9f1359088
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-11-03 18:13:01 +0530

    chore: make refresh tokens false   * change authProvider in plugin itself

--- plugins/auth-auth0/src/events/createAdminAppModule.ts
+++ plugins/auth-auth0/src/events/createAdminAppModule.ts
@@ -5,11 +5,18 @@ import {
 import { readFile } from "@amplication/code-gen-utils";
 import { templatesPath } from "../constants";
 import { join } from "path";
+import { EnumAuthProviderType } from "@amplication/code-gen-types/src/models";
 
 export const beforeCreateAdminAppModule = async (
   context: DsgContext,
   eventParams: CreateAdminAppModuleParams,
 ): Promise<CreateAdminAppModuleParams> => {
+  const { resourceInfo } = context;
+  if(resourceInfo) {
+    resourceInfo.settings.authProvider = EnumAuthProviderType.Auth0;
+  }
+
+
   const _appTemplate = await readFile(join(templatesPath, "App.template.tsx"));
 
   return {
