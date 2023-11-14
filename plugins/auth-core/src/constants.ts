d6d9f2f3015bb2673887b1059216c25053ce71df
Amit Barletz <amit@amplication.com>
2023-11-14 20:40:16 +0200

    refactor(auth-core): throwing an error when missing required auth entity fields

--- plugins/auth-core/src/constants.ts
+++ plugins/auth-core/src/constants.ts
@@ -10,3 +10,4 @@ export const templatesPath = resolve(__dirname, "./templates");
 export const AUTH_ENTITY_ERROR = "Authentication entity does not exist";
 export const AUTH_ENTITY_LOG_ERROR =
   "Authentication entity does not exist. Have you configured the authentication entity in the Resource Settings?";
+  export const AUTH_ENTITY_FIELD_ROLES = "roles";
