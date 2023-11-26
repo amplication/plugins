b7c724e35c15e2f6b14a6eee4217163757584159
ChenemiKen <chenemiken15@gmail.com>
2023-11-26 12:46:30 +0100

    fixed isuue with getSecret type mismatch

--- plugins/secrets-manager-bitwarden/src/static/startup/providers/secrets/secretsManager.service.ts
+++ plugins/secrets-manager-bitwarden/src/static/startup/providers/secrets/secretsManager.service.ts
@@ -1,18 +1,19 @@
 import { Injectable, Inject } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { SecretsManagerServiceBase } from "./base/secretsManager.service.base";
+import { EnumSecretsNameKey } from "./secretsNameKey.enum";
 
 @Injectable()
 export class SecretsManagerService extends SecretsManagerServiceBase {
   constructor(
     @Inject("BITWARDEN_SECRETS_MANAGER")
-    protected readonly secrets: Partial<Record<string, unknown>>,
+    protected readonly secrets: Partial<Record<EnumSecretsNameKey, unknown>>,
     protected readonly configService: ConfigService
   ) {
     super(configService);
   }
 
-  async getSecret<T>(key: string): Promise<T | null> {
+  async getSecret<T>(key: EnumSecretsNameKey): Promise<T | null> {
     return this.secrets[key] as any
   }
 }
--- plugins/secrets-manager-bitwarden/src/static/startup/providers/secrets/secretsManagerFactory.ts
+++ plugins/secrets-manager-bitwarden/src/static/startup/providers/secrets/secretsManagerFactory.ts
@@ -1,17 +1,16 @@
 import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { 
-    BitwardenClient, ClientSettings, DeviceType, LogLevel,SecretIdentifierResponse 
+import {
+    BitwardenClient, ClientSettings, DeviceType, LogLevel, SecretIdentifierResponse
 } from "@bitwarden/sdk-napi";
 import { EnumSecretsNameKey } from "./secretsNameKey.enum";
 
 export const secretsManagerFactory = {
     provide: "BITWARDEN_SECRETS_MANAGER",
     useFactory: async (configService: ConfigService) => {
-        console.log("running factory")
         const logger = new Logger()
         const settings: ClientSettings = {
-            apiUrl : configService.get("BITWARDEN_API_URL"),
+            apiUrl: configService.get("BITWARDEN_API_URL"),
             identityUrl: configService.get("BITWARDEN_IDENTITY_URL"),
             userAgent: "Bitwarden SDK",
             deviceType: DeviceType.SDK,
@@ -21,32 +20,37 @@ export const secretsManagerFactory = {
         const bitwardenClient = new BitwardenClient(settings, LogLevel.Info)
 
         const auth = await bitwardenClient.loginWithAccessToken(accessToken);
-        if(!auth.success){
-            logger.error(`Bitwarden authentication failed: ${auth.errorMessage}`, "SecretsManager")
+        if (!auth.success) {
+            logger.error(`Bitwarden authentication failed: ${auth.errorMessage}`,
+                "SecretsManager")
             // throw Error(`Bitwarden authentication failed: ${auth.errorMessage}`);
         }
 
         const secretsList = await bitwardenClient.secrets().list(organisationId);
-        if(!secretsList.success){
-            logger.error(`Error while fetching secrets : ${secretsList.errorMessage}`, "SecretsManager")
+        if (!secretsList.success) {
+            logger.error(`Error while fetching secrets : ${secretsList.errorMessage}`,
+                "SecretsManager")
             // throw Error(`Error while fetching secrets : ${secretsList.errorMessage}`);
         }
 
-        const secretsNamesIds: string[] = [] 
-        secretsList.data?.data.forEach((sec:SecretIdentifierResponse) => {
-            if(Object.values(EnumSecretsNameKey).includes(sec.key)) secretsNamesIds.push(sec.id)
+        const secretsNamesIds: string[] = []
+        secretsList.data?.data.forEach((sec: SecretIdentifierResponse) => {
+            if (Object.values(EnumSecretsNameKey).includes(sec.key as EnumSecretsNameKey)) {
+                secretsNamesIds.push(sec.id)
+            }
         })
 
         const response = await bitwardenClient.secrets().getByIds(secretsNamesIds)
-        if(response.success){
-            logger.error(`Error fetching secrets from secretManager :${response.errorMessage}`, "SecretsManager")
+        if (response.success) {
+            logger.error(`Error fetching secrets from secretManager :
+                ${response.errorMessage}`, "SecretsManager")
             // throw Error(`Error fetching secrets from secretManager :${response.errorMessage}`)
         }
-        
+
         var secrets: Partial<Record<EnumSecretsNameKey, unknown>> = {}
-        
-        if(response.data){
-            for(const secret of response.data?.data){
+
+        if (response.data) {
+            for (const secret of response.data?.data) {
                 secrets = {
                     ...secrets,
                     [secret.key]: secret.value
