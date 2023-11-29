e56e3b60709d3f946c45aca296d1c3a7fb20c4cd
levivannoort <levivnoort@gmail.com>
2023-11-29 11:15:34 +0100

    chore: add top level project for storing artifacts across the organisation

--- plugins/provisioning-terraform-gcp-core/src/static/projects-common.tf
+++ plugins/provisioning-terraform-gcp-core/src/static/projects-common.tf
@@ -45,3 +45,26 @@ module "host_project_monitoring" {
     module.folders_top_level,
   ]
 }
+
+// https://registry.terraform.io/modules/terraform-google-modules/project-factory/google/14.4.0
+module "host_project_artifacts" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "14.4.0"
+
+  random_project_id = true
+
+  name       = "artifacts"
+  project_id = "artifacts"
+  org_id     = var.organization_id
+  folder_id  = module.folders_top_level.ids["common"]
+
+  billing_account = var.billing_account
+
+  activate_apis = [
+    "artifactregistry.googleapis.com",
+  ]
+
+  depends_on = [
+    module.folders_top_level,
+  ]
+}
