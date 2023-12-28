256a1c95ced062c29a2234791f95dc8df166e504
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-12-28 15:00:00 +0530

    chore: add default settings

--- plugins/auth-keycloak/src/types.ts
+++ plugins/auth-keycloak/src/types.ts
@@ -1,18 +1,18 @@
 export interface Settings {
   port: number;
-  realmName?: string;
   realmID: string;
+  clientID: string;
+  recipe: IRecipe;
+  realmName?: string;
   clientName?: string;
   clientDescription?: string;
-  clientID: string;
   adminUsername?: string;
   adminPassword?: string;
-  recipe: IRecipe;
   defaultUser?: Record<string, unknown>;
 }
 
 export interface IRecipe {
-  emailFieldName: string;
+  emailFieldName?: string;
   verifyEmail: boolean;
   registrationAllowed: boolean;
   payloadFieldMapping: Record<string, string>;
