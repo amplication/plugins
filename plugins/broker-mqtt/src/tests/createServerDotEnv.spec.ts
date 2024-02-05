0795862d4f74ae0f9ae065f8299e940e5acad069
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-02-05 18:18:46 +0530

    feat(test): add server dot env

---
+++ plugins/broker-mqtt/src/tests/createServerDotEnv.spec.ts
@@ -0,0 +1,69 @@
+import {
+  CreateServerDotEnvParams,
+  DsgContext,
+} from "@amplication/code-gen-types";
+import { beforeCreateServerDotEnv } from "../events/createServerDotEnv";
+import { mock } from "jest-mock-extended";
+
+describe("Testing beforeCreateServerDotEnv hook", () => {
+  let context: DsgContext;
+  let eventParams: CreateServerDotEnvParams;
+
+  beforeEach(() => {
+    context = mock<DsgContext>({
+      pluginInstallations: [
+        {
+          npm: "@amplication/plugin-broker-mqtt",
+        },
+      ],
+    });
+    eventParams = mock<CreateServerDotEnvParams>({
+      envVariables: [],
+    });
+  });
+
+  it("should use default values if plugin settings are not defined", async () => {
+    eventParams = await beforeCreateServerDotEnv(context, eventParams);
+
+    const expectedEnvVariables = [
+      { MQTT_BROKER_HOST: "localhost" },
+      { MQTT_WEB_UI_PORT: "8080" },
+      { MQTT_PORT: "1883" },
+      { MQTT_WS_PORT: "8073" },
+      { MQTT_USERNAME: "admin" },
+      { MQTT_PASSWORD: "admin" },
+      { MQTT_CLIENT_ID: "broker-mqtt-undefined" },
+    ];
+
+    expect(eventParams.envVariables).toEqual(expectedEnvVariables);
+  });
+
+  it("should use plugin settings if defined", async () => {
+    context.pluginInstallations[0].settings = {
+      mqttBrokerHost: "CUSTOM_MQTT_BROKER_HOST",
+      mqttWebUiPort: 1234,
+      mqttPort: 5678,
+      mqttWsPort: 9012,
+      mqttUsername: "CUSTOM_MQTT_USERNAME",
+      mqttPassword: "CUSTOM_MQTT_PASSWORD",
+    };
+
+    if (context.resourceInfo) {
+      context.resourceInfo.id = "123";
+    }
+
+    eventParams = await beforeCreateServerDotEnv(context, eventParams);
+
+    const expectedEnvVariables = [
+      { MQTT_BROKER_HOST: "CUSTOM_MQTT_BROKER_HOST" },
+      { MQTT_WEB_UI_PORT: "1234" },
+      { MQTT_PORT: "5678" },
+      { MQTT_WS_PORT: "9012" },
+      { MQTT_USERNAME: "CUSTOM_MQTT_USERNAME" },
+      { MQTT_PASSWORD: "CUSTOM_MQTT_PASSWORD" },
+      { MQTT_CLIENT_ID: "broker-mqtt-123" },
+    ];
+
+    expect(eventParams.envVariables).toEqual(expectedEnvVariables);
+  });
+});
