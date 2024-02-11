990ec34245b27a1b57957544662aa220ad069b90
pyxelpioneer <151645921+pyxelpioneer@users.noreply.github.com>
2024-02-11 21:59:14 +0530

    Update

--- plugins/devcontainer-github-codespaces/src/utils/genDevcontainerConfig.ts
+++ plugins/devcontainer-github-codespaces/src/utils/genDevcontainerConfig.ts
@@ -1,13 +1,15 @@
 import path from "path"
+import countParents from "./countParents"
 
 export default function genDevcontainerConfig(serviceName: string, serverRoot: string, devcontainerPath: string) {
-  const initSHPath = escapeSpace(`${path.dirname(devcontainerPath)}/init.sh`)
+  const initSHPath = `${path.dirname(devcontainerPath)}/init.sh`
+  const parents = countParents(devcontainerPath)
 
   return {
     "name": serviceName,
     "service": "server",
     "dockerComposeFile": [
-      `../${serverRoot}/docker-compose.yml`
+      `${"../".repeat(parents)}${serverRoot}/docker-compose.yml`
     ],
     "features": {
       "ghcr.io/devcontainers/features/docker-in-docker:2": {}
