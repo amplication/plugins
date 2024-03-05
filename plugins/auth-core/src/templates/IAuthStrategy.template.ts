82374740e3d679b1ca6e6eee94d620e25f44e462
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-03-05 17:10:38 +0000

    chore(auth-core): remove old auth-core plugin

--- plugins/auth-core/src/templates/IAuthStrategy.template.ts
+++
@@ -1,6 +0,0 @@
-declare class ENTITY_NAME_INFO {}
-
-export interface IAuthStrategy {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  validate: (...any: any) => Promise<ENTITY_NAME_INFO>;
-}
