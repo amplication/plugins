52078bf3e8652c74dbb870460242e9ec57e76f76
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-03-07 09:33:13 +0000

    chore(saml): fix environment variables

--- plugins/auth-saml/src/events/create-server-dot-env.ts
+++ plugins/auth-saml/src/events/create-server-dot-env.ts
@@ -11,11 +11,14 @@ export function beforeCreateServerDotEnv(
   // add default auth-core dependencies
   eventParams = authCoreBeforeCreateServerDotEnv(context, eventParams);
   const vars = {
-    SAML_ENTRY_POINT: "${SAML_ENTRY_POINT}",
-    SAML_ISSUER: "${SAML_ISSUER}",
-    SAML_CERT: "${SAML_CERT}",
-    SAML_PUBLIC_CERT: "${SAML_PUBLIC_CERT}",
-    SAML_REDIRECT_CALLBACK_URL: "${SAML_REDIRECT_CALLBACK_URL}",
+    SAML_ENTRY_POINT:
+      "# URL that goes from the Service Provider -> Identity Provider",
+    SAML_ISSUER: "# issuer string to supply to identity provider. EntityID",
+    SAML_DECRYPT_KEY: "# Service Provider response decryption key",
+    SAML_PRIVATE_CERT: "# Service Provider private Certificate",
+    SAML_PUBLIC_CERT: "# Identity Provider's public key",
+    SAML_REDIRECT_CALLBACK_URL:
+      "# Service Provider callback URL to client that will receive the access token",
   };
   const newEnvParams = [
     ...eventParams.envVariables,
