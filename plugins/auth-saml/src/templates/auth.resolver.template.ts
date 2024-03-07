e3b285dbc64433ba41ed85a91178adab48c31c40
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-03-07 15:08:07 +0000

    fix(saml): auth resolver templates

---
+++ plugins/auth-saml/src/templates/auth.resolver.template.ts
@@ -0,0 +1,20 @@
+import * as common from "@nestjs/common";
+import { Query, Resolver } from "@nestjs/graphql";
+import * as gqlACGuard from "../auth/gqlAC.guard";
+import { GqlDefaultAuthGuard } from "./gqlDefaultAuth.guard";
+import { UserData } from "./userData.decorator";
+
+declare class ENTITY_NAME_INFO {}
+
+@Resolver(ENTITY_NAME_INFO)
+export class AuthResolver {
+  constructor() {}
+
+  @Query(() => ENTITY_NAME_INFO)
+  @common.UseGuards(GqlDefaultAuthGuard, gqlACGuard.GqlACGuard)
+  async ENTITY_NAME(
+    @UserData() entityInfo: ENTITY_NAME_INFO
+  ): Promise<ENTITY_NAME_INFO> {
+    return entityInfo;
+  }
+}
