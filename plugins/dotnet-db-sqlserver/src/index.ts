e55f9b44866f0ec36b2da93043875a5086607106
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-05-21 16:44:52 +0100

    chore(dotnet-db-mssql): add logic to interact with appsettings and program.cs generation

--- plugins/dotnet-db-sqlserver/src/index.ts
+++ plugins/dotnet-db-sqlserver/src/index.ts
@@ -3,10 +3,15 @@ import {
   dotnetPluginEventsParams as dotnet,
   dotnetTypes,
 } from "@amplication/code-gen-types";
+import { ClassReference, CodeBlock } from "@amplication/csharp-ast";
 import {
   updateDockerComposeDevProperties,
   updateDockerComposeProperties,
 } from "./constants";
+import { getPluginSettings } from "./utils";
+import { pascalCase } from "pascal-case";
+
+const CONNECTION_STRING = "DefaultConnection";
 
 class MSSQLServerPlugin implements dotnetTypes.AmplicationPlugin {
   register(): dotnetPluginEventsTypes.DotnetEvents {
@@ -17,11 +22,20 @@ class MSSQLServerPlugin implements dotnetTypes.AmplicationPlugin {
       CreateServerDockerComposeDev: {
         before: this.beforeCreateServerDockerComposeDev,
       },
+      CreateServerCsproj: {
+        before: this.beforeCreateServerCsproj,
+      },
+      CreateServerAppsettings: {
+        before: this.beforeCreateServerAppsettings,
+      },
+      CreateProgramFile: {
+        before: this.beforeCreateProgramFile,
+      },
     };
   }
 
   beforeCreateServerDockerCompose(
-    context: dotnetTypes.DsgContext,
+    _: dotnetTypes.DsgContext,
     eventParams: dotnet.CreateServerDockerComposeParams
   ) {
     eventParams.updateProperties.push(...updateDockerComposeProperties);
@@ -29,10 +43,64 @@ class MSSQLServerPlugin implements dotnetTypes.AmplicationPlugin {
   }
 
   beforeCreateServerDockerComposeDev(
-    context: dotnetTypes.DsgContext,
+    _: dotnetTypes.DsgContext,
     eventParams: dotnet.CreateServerDockerComposeDevParams
   ) {
     eventParams.updateProperties.push(...updateDockerComposeDevProperties);
+    return eventParams;
+  }
+
+  beforeCreateServerCsproj(
+    _: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateServerCsprojParams
+  ) {
+    eventParams.packageReferences.push({
+      include: "Microsoft.EntityFrameworkCore.SqlServer",
+      version: "8.0.5",
+    });
+
+    return eventParams;
+  }
+
+  beforeCreateServerAppsettings(
+    context: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateServerAppsettingsParams
+  ) {
+    const { port, password, user, host, dbName } = getPluginSettings(
+      context.pluginInstallations
+    );
+
+    eventParams.updateProperties = {
+      ...eventParams.updateProperties,
+      ConnectionStrings: {
+        [CONNECTION_STRING]: `sqlserver://${host}:${port};database=${dbName};user=${user};password=${password};TrustServerCertificate=true`,
+      },
+    };
+    return eventParams;
+  }
+
+  beforeCreateProgramFile(
+    { resourceInfo }: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateProgramFileParams
+  ) {
+    const serviceNamespace = pascalCase(resourceInfo?.name ?? "");
+
+    eventParams.builderServicesBlocks.push(
+      new CodeBlock({
+        code: `builder.services.AddDbContext<DbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("${CONNECTION_STRING}")));`,
+        references: [
+          new ClassReference({
+            name: "AddDbContext",
+            namespace: "Microsoft.EntityFrameworkCore",
+          }),
+          new ClassReference({
+            name: "DbContext",
+            namespace: `${serviceNamespace}.Infrastructure`,
+          }),
+        ],
+      })
+    );
+
     return eventParams;
   }
 }
