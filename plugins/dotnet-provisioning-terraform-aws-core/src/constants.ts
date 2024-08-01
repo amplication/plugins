3ba39c0332af139483e23bd760924c413653e103
Haim Bell <bl.haim@gmail.com>
2024-08-01 15:00:31 +0300

    feat(plugin-dotnet-provisioning-terraform-aws-core): add plugin

---
+++ plugins/dotnet-provisioning-terraform-aws-core/src/constants.ts
@@ -0,0 +1,16 @@
+// global constants
+export const nameKey = "${{ NAME }}";
+export const regionIdentifierKey = "${{ REGION_IDENTIFIER }}";
+export const environmentKey = "${{ ENVIRONMENT }}";
+
+// vpc constants
+export const vpcCidrBlockKey = "${{ CIDR_BLOCK }}";
+export const createDatabaseSubnetGroupKey =
+  "${{ CREATE_DATABASE_SUBNET_GROUP }}";
+export const enableDnsHostnamesKey = "${{ ENABLE_DNS_HOSTNAMES }}";
+export const enableDnsSupportKey = "${{ ENABLE_DNS_SUPPORT }}";
+export const enableNatGatewayKey = "${{ ENABLE_NAT_GATEWAY }}";
+export const singleNatGatewayKey = "${{ SINGLE_NAT_GATEWAY }}";
+
+// backend constants
+export const backendKey = "${{ BACKEND }}";
