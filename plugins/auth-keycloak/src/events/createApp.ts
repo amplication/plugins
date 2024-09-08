a8f9d1dad9d6a9e96fd0044940612804ebf1aa38
Mor Hagbi <mor@amplication.com>
2024-09-08 18:28:52 +0300

    refactor(keycloak): add wait to merge function

--- plugins/auth-keycloak/src/events/createApp.ts
+++ plugins/auth-keycloak/src/events/createApp.ts
@@ -16,6 +16,6 @@ export const afterCreateAdminApp = async (
   );
 
   // Merge the static files with the existing modules replacing any existing files
-  modules.merge(staticFiles);
+  await modules.merge(staticFiles);
   return modules;
 };
