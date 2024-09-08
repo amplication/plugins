94613d9756b148cf6b7c14422cf79dbf614e61b3
Mor Hagbi <mor@amplication.com>
2024-09-08 18:02:42 +0300

    feat(auth-0): replace LoginForm component instead Login component

---
+++ plugins/auth-auth0/src/static/client/LoginForm.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useLogin, useNotify } from "react-admin";
+import { Button } from "@mui/material";
+import "./login.scss";
+
+const LoginForm = ({ theme }: any) => {
+  const login = useLogin();
+  const notify = useNotify();
+  const submit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    notify("Redirecting to Auth0");
+    login({});
+  };
+
+
+  return (
+    <Button
+    type="submit"
+    variant="contained"
+    color="primary"
+    onClick={submit}
+  >
+    Log in
+  </Button>
+  );
+};
+
+export default LoginForm;
