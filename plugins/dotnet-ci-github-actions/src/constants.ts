3dc4fcbee44f8fbfa252e1e5bba73a22b97c6ff5
Haim Bell <bl.haim@gmail.com>
2024-09-16 14:26:51 +0300

    feat(dotnet-ci-github): add plugin

---
+++ plugins/dotnet-ci-github-actions/src/constants.ts
@@ -0,0 +1,5 @@
+export const serviceNameKey = "${{ SERVICE_NAME }}";
+export const imageKey = "${{ IMAGE }}";
+export const serviceWorkingDirectoryKey = "${{ SERVICE_DIRECTORY }}";
+export const authenticationUsernameKey = "${{ AUTHENTICATION_USERNAME }}";
+export const authenticationPasswordKey = "${{ AUTHENTICATION_PASSWORD }}";
