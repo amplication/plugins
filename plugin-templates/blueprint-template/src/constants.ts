aa015772e26aa2c5ace387f88b331e3a28b4d090
Itai Nathaniel <itainathaniel@gmail.com>
2025-03-02 12:33:43 +0200

    add template content
    Signed-off-by: Itai Nathaniel <itainathaniel@gmail.com>

---
+++ plugin-templates/blueprint-template/src/constants.ts
@@ -0,0 +1,10 @@
+export const REPLACEMENTS: Record<string, string> = {
+  PLUGIN_KEBAB_CASE_NAME: "blueprint-plugin-name-placeholder",
+  PLUGIN_CAMEL_CASE_NAME: "BlueprintPluginNamePlaceholder",
+  PLUGIN_DISPLAY_NAME: "Blueprint Plugin Name Placeholder",
+  PLUGIN_DESCRIPTION: "A blueprint plugin description placeholder.",
+  PLUGIN_AUTHOR: "Blueprint Plugin Author Placeholder",
+  PLUGIN_LICENSE: "Apache-2.0",
+  COPY_PLUGIN_SETTINGS: "{}",
+  PLUGIN_DOMAIN: "",
+};
